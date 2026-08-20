cdb59cd05b350a7052e72f32f07d8eddfe655fb4
Justin <justin.blair.rankin@gmail.com>
2026-08-20 17:41:41 +1000

    deps: update deps-policy versions

--- src/policy/base.deps.ts
+++ src/policy/base.deps.ts
@@ -39,7 +39,7 @@ const hooks: Record<string, string> = {
 };
 
 const ecosystem: Record<string, string> = {
-  '@finografic/project-scripts': '^2.0.0',
+  '@finografic/project-scripts': '^2.0.1',
 };
 
 /**
